17ae7f3d82ea64f35608816371f1683fb508873d
Phanindra Ganti <pganti@linkedin.com>
2013-01-30 10:02:59 -0800

    Add Apache 2.0 copyright to all files

--- databus-group-leader/databus-group-leader-example/src/main/java/com/linkedin/databus/groupleader/example/simple/LeaderElectSimpleExample.java
+++ databus-group-leader/databus-group-leader-example/src/main/java/com/linkedin/databus/groupleader/example/simple/LeaderElectSimpleExample.java
@@ -1,6 +1,25 @@
 /**
  * $Id: LeaderElectSimpleExample.java 261005 2011-04-15 00:36:51Z snagaraj $ */
 package com.linkedin.databus.groupleader.example.simple;
+/*
+ *
+ * Copyright 2013 LinkedIn Corp. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
 
 import org.apache.log4j.Logger;
 
