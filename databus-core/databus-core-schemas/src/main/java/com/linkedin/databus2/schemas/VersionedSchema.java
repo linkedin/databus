1a0ddc187d7287185a7aeb4d48e7325f57f6df36
buptyzc <buptyzc@163.com>
2017-05-29 16:05:41 +0800

    cache pk field and get key obj from record (#97)

--- databus-core/databus-core-schemas/src/main/java/com/linkedin/databus2/schemas/VersionedSchema.java
+++ databus-core/databus-core-schemas/src/main/java/com/linkedin/databus2/schemas/VersionedSchema.java
@@ -19,6 +19,9 @@ package com.linkedin.databus2.schemas;
 */
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.avro.Schema;
 
 /**
@@ -30,12 +33,14 @@ public class VersionedSchema
   private final VersionedSchemaId _id;
 
   private final String _origSchemaStr;
+  private final List<Schema.Field> _pkFieldList;
 
   public VersionedSchema(VersionedSchemaId id, Schema s, String origSchemaStr)
   {
     _schema = s;
     _id = id;
     _origSchemaStr = origSchemaStr;
+    _pkFieldList = new ArrayList<Schema.Field>();
   }
 
   public VersionedSchema(String baseName, short id, Schema s, String origSchemaStr)
@@ -84,6 +89,11 @@ public class VersionedSchema
   public VersionedSchemaId getId()
   {
     return _id;
+  }
+  
+  public List<Schema.Field> getPkFieldList()
+  {
+	return _pkFieldList;
   }
 
   /**
